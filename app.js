173c56bb88ce9914cad0a0280b0c3e0770ef0d8d
umeshmehta14 <ishaanmehta782@gmail.com>
2022-09-19 23:55:50 +0530

    done

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtI = document.querySelector("#tta");
 var outputDiv = document.querySelector("#output");
 
-btntranslate.addEventListener("click", function clickEventHandler(){
-    outputDiv.innerText = "wefreggreggv:" + txtI.value
-})
+// var serurl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serurl = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+
+function gettranslate(text){
+    return serurl + "?" + "text=" + text;
+}
+
+function erroHandler(error) {
+    console.log("error occurred", error);
+    alert("something wrong with server");
+    
+}
+function clickHandler(){
+    // outputDiv.innerText = "wefreggreggv:" + txtI.value
+    var Itxt = txtI.value;
+
+    // calling server for processing
+    fetch(gettranslate(Itxt))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        // console.log(json.contents.translated)
+    })
+    .catch(erroHandler);
+}
+btntranslate.addEventListener("click", clickHandler)
